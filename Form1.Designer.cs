90403fae76b9e99bcbe2a80a2bb678253a3287ea
Youngfun <youngfun429@hotmail.com>
2025-07-29 14:21:33 +0800

    First Commit
    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace PassGen_10Prv
+{
+    partial class FrmPassGen_10Prv
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TxtPassword_10Prv = new TextBox();
+            BtnCopy_10Prv = new Button();
+            NumLen_10Prv = new NumericUpDown();
+            LblLen_10Prv = new Label();
+            ChkUpper_10Prv = new CheckBox();
+            ChkLower_10Prv = new CheckBox();
+            ChkNum_10Prv = new CheckBox();
+            ChkOthers_10Prv = new CheckBox();
+            TxtOthers_10Prv = new TextBox();
+            BtnGen_10Prv = new Button();
+            BtnResetOth_10Prv = new Button();
+            ((System.ComponentModel.ISupportInitialize)NumLen_10Prv).BeginInit();
+            SuspendLayout();
+            // 
+            // TxtPassword_10Prv
+            // 
+            TxtPassword_10Prv.BorderStyle = BorderStyle.FixedSingle;
+            TxtPassword_10Prv.Enabled = false;
+            TxtPassword_10Prv.Location = new Point(39, 22);
+            TxtPassword_10Prv.Name = "TxtPassword_10Prv";
+            TxtPassword_10Prv.Size = new Size(722, 23);
+            TxtPassword_10Prv.TabIndex = 0;
+            TxtPassword_10Prv.TabStop = false;
+            TxtPassword_10Prv.Text = "PassWord";
+            TxtPassword_10Prv.TextAlign = HorizontalAlignment.Center;
+            // 
+            // BtnCopy_10Prv
+            // 
+            BtnCopy_10Prv.Enabled = false;
+            BtnCopy_10Prv.Font = new Font("Microsoft YaHei UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 134);
+            BtnCopy_10Prv.Location = new Point(226, 125);
+            BtnCopy_10Prv.Name = "BtnCopy_10Prv";
+            BtnCopy_10Prv.Size = new Size(349, 59);
+            BtnCopy_10Prv.TabIndex = 1;
+            BtnCopy_10Prv.Text = "复制生成的密码(&P)";
+            BtnCopy_10Prv.UseVisualStyleBackColor = true;
+            BtnCopy_10Prv.Click += BtnCopy_10Prv_Click;
+            // 
+            // NumLen_10Prv
+            // 
+            NumLen_10Prv.Location = new Point(379, 198);
+            NumLen_10Prv.Maximum = new decimal(new int[] { 50, 0, 0, 0 });
+            NumLen_10Prv.Minimum = new decimal(new int[] { 4, 0, 0, 0 });
+            NumLen_10Prv.Name = "NumLen_10Prv";
+            NumLen_10Prv.Size = new Size(120, 23);
+            NumLen_10Prv.TabIndex = 3;
+            NumLen_10Prv.TextAlign = HorizontalAlignment.Right;
+            NumLen_10Prv.Value = new decimal(new int[] { 15, 0, 0, 0 });
+            // 
+            // LblLen_10Prv
+            // 
+            LblLen_10Prv.AutoSize = true;
+            LblLen_10Prv.Location = new Point(301, 204);
+            LblLen_10Prv.Name = "LblLen_10Prv";
+            LblLen_10Prv.Size = new Size(71, 17);
+            LblLen_10Prv.TabIndex = 2;
+            LblLen_10Prv.Text = "密码长度(&E)";
+            // 
+            // ChkUpper_10Prv
+            // 
+            ChkUpper_10Prv.AutoSize = true;
+            ChkUpper_10Prv.Checked = true;
+            ChkUpper_10Prv.CheckState = CheckState.Checked;
+            ChkUpper_10Prv.Location = new Point(349, 235);
+            ChkUpper_10Prv.Name = "ChkUpper_10Prv";
+            ChkUpper_10Prv.Size = new Size(120, 21);
+            ChkUpper_10Prv.TabIndex = 4;
+            ChkUpper_10Prv.Text = "大写字母(A-Z)(&U)";
+            ChkUpper_10Prv.UseVisualStyleBackColor = true;
+            ChkUpper_10Prv.CheckedChanged += ChkUpper_10Prv_CheckedChanged;
+            // 
+            // ChkLower_10Prv
+            // 
+            ChkLower_10Prv.AutoSize = true;
+            ChkLower_10Prv.Checked = true;
+            ChkLower_10Prv.CheckState = CheckState.Checked;
+            ChkLower_10Prv.Location = new Point(350, 270);
+            ChkLower_10Prv.Name = "ChkLower_10Prv";
+            ChkLower_10Prv.Size = new Size(115, 21);
+            ChkLower_10Prv.TabIndex = 5;
+            ChkLower_10Prv.Text = "小写字母(a-z)(&L)";
+            ChkLower_10Prv.UseVisualStyleBackColor = true;
+            ChkLower_10Prv.CheckedChanged += ChkLower_10Prv_CheckedChanged;
+            // 
+            // ChkNum_10Prv
+            // 
+            ChkNum_10Prv.AutoSize = true;
+            ChkNum_10Prv.Checked = true;
+            ChkNum_10Prv.CheckState = CheckState.Checked;
+            ChkNum_10Prv.Location = new Point(361, 305);
+            ChkNum_10Prv.Name = "ChkNum_10Prv";
+            ChkNum_10Prv.Size = new Size(96, 21);
+            ChkNum_10Prv.TabIndex = 6;
+            ChkNum_10Prv.Text = "数字(0-9)(&N)";
+            ChkNum_10Prv.UseVisualStyleBackColor = true;
+            ChkNum_10Prv.CheckedChanged += ChkNum_10Prv_CheckedChanged;
+            // 
+            // ChkOthers_10Prv
+            // 
+            ChkOthers_10Prv.AutoSize = true;
+            ChkOthers_10Prv.Checked = true;
+            ChkOthers_10Prv.CheckState = CheckState.Checked;
+            ChkOthers_10Prv.Location = new Point(102, 342);
+            ChkOthers_10Prv.Name = "ChkOthers_10Prv";
+            ChkOthers_10Prv.Size = new Size(93, 21);
+            ChkOthers_10Prv.TabIndex = 7;
+            ChkOthers_10Prv.Text = "特殊字符(&O)";
+            ChkOthers_10Prv.UseVisualStyleBackColor = true;
+            ChkOthers_10Prv.CheckedChanged += ChkOthers_10Prv_CheckedChanged;
+            // 
+            // TxtOthers_10Prv
+            // 
+            TxtOthers_10Prv.BorderStyle = BorderStyle.FixedSingle;
+            TxtOthers_10Prv.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 134);
+            TxtOthers_10Prv.Location = new Point(212, 340);
+            TxtOthers_10Prv.Name = "TxtOthers_10Prv";
+            TxtOthers_10Prv.Size = new Size(310, 28);
+            TxtOthers_10Prv.TabIndex = 0;
+            TxtOthers_10Prv.TabStop = false;
+            TxtOthers_10Prv.Text = "~!@#$%^&*+-/.,\\{}[]();:";
+            TxtOthers_10Prv.TextChanged += TxtOthers_10Prv_TextChanged;
+            // 
+            // BtnGen_10Prv
+            // 
+            BtnGen_10Prv.Font = new Font("Microsoft YaHei UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 134);
+            BtnGen_10Prv.Location = new Point(213, 377);
+            BtnGen_10Prv.Name = "BtnGen_10Prv";
+            BtnGen_10Prv.Size = new Size(374, 59);
+            BtnGen_10Prv.TabIndex = 9;
+            BtnGen_10Prv.Text = "生成随机密码(&G)";
+            BtnGen_10Prv.UseVisualStyleBackColor = true;
+            BtnGen_10Prv.Click += BtnGen_10Prv_Click;
+            // 
+            // BtnResetOth_10Prv
+            // 
+            BtnResetOth_10Prv.Location = new Point(539, 340);
+            BtnResetOth_10Prv.Name = "BtnResetOth_10Prv";
+            BtnResetOth_10Prv.Size = new Size(160, 23);
+            BtnResetOth_10Prv.TabIndex = 10;
+            BtnResetOth_10Prv.Text = "重置特殊字符(&R)";
+            BtnResetOth_10Prv.UseVisualStyleBackColor = true;
+            BtnResetOth_10Prv.Click += BtnResetOth_10Prv_Click;
+            // 
+            // FrmPassGen_10Prv
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(BtnResetOth_10Prv);
+            Controls.Add(BtnGen_10Prv);
+            Controls.Add(TxtOthers_10Prv);
+            Controls.Add(ChkOthers_10Prv);
+            Controls.Add(ChkNum_10Prv);
+            Controls.Add(ChkLower_10Prv);
+            Controls.Add(ChkUpper_10Prv);
+            Controls.Add(LblLen_10Prv);
+            Controls.Add(NumLen_10Prv);
+            Controls.Add(BtnCopy_10Prv);
+            Controls.Add(TxtPassword_10Prv);
+            MaximizeBox = false;
+            Name = "FrmPassGen_10Prv";
+            Text = "随机密码生成器_10Prv";
+            ((System.ComponentModel.ISupportInitialize)NumLen_10Prv).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox TxtPassword_10Prv;
+        private Button BtnCopy_10Prv;
+        private NumericUpDown NumLen_10Prv;
+        private Label LblLen_10Prv;
+        private CheckBox ChkUpper_10Prv;
+        private CheckBox ChkLower_10Prv;
+        private CheckBox ChkNum_10Prv;
+        private CheckBox ChkOthers_10Prv;
+        private TextBox TxtOthers_10Prv;
+        private Button BtnGen_10Prv;
+        private Button BtnResetOth_10Prv;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,273 @@
+namespace PassGen_10Prv
+{
+    public partial class FrmPassGen_10Prv : Form
+    {
+        bool bolUpper = true, bolLower = true, bolNum = true, bolOth = true;
+        public FrmPassGen_10Prv()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCopy_10Prv_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(TxtPassword_10Prv.Text);
+        }
+
+        private void ChkUpper_10Prv_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ChkUpper_10Prv.Checked)
+            {
+                bolUpper = true;
+                BtnGen_10Prv.Enabled = true;
+                BtnGen_10Prv.Text = "重新生成随机密码(&G)";
+                EnableChkBox();
+            }
+            else
+            {
+                bolUpper = false;
+                DisableChkBox(bolUpper, bolLower, bolNum, bolOth);
+            }
+        }
+
+        private void ChkLower_10Prv_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ChkLower_10Prv.Checked)
+            {
+                bolLower = true;
+                BtnGen_10Prv.Enabled = true;
+                BtnGen_10Prv.Text = "重新生成随机密码(&G)";
+                EnableChkBox();
+            }
+            else
+            {
+                bolLower = false;
+                DisableChkBox(bolUpper, bolLower, bolNum, bolOth);
+            }
+        }
+
+        private void ChkNum_10Prv_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ChkNum_10Prv.Checked)
+            {
+                bolNum = true;
+                BtnGen_10Prv.Enabled = true;
+                BtnGen_10Prv.Text = "重新生成随机密码(&G)";
+                EnableChkBox();
+            }
+            else
+            {
+                bolNum = false;
+                DisableChkBox(bolUpper, bolLower, bolNum, bolOth);
+            }
+        }
+
+        private void ChkOthers_10Prv_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ChkOthers_10Prv.Checked)
+            {
+                bolOth = true;
+                TxtOthers_10Prv.Enabled = true;
+                BtnGen_10Prv.Enabled = true;
+                BtnGen_10Prv.Text = "重新生成随机密码(&G)";
+                EnableChkBox();
+            }
+            else
+            {
+                bolOth = false;
+                TxtOthers_10Prv.Enabled = false;
+                BtnResetOth_10Prv.Enabled = false;
+                DisableChkBox(bolUpper, bolLower, bolNum, bolOth);
+            }
+        }
+
+        private void BtnGen_10Prv_Click(object sender, EventArgs e)
+        {
+            bool bolInclNonOthers = false;
+            char[] chars =
+                {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+                '1','2','3','4','5','6','7','8','9','0',' '
+                };
+            for (int j = 0; j < TxtOthers_10Prv.Text.Length; j++)
+            {
+                if (((int)TxtOthers_10Prv.Text[j] > 127) | (TxtOthers_10Prv.Text.IndexOfAny(chars) >= 0))
+                {
+                    MessageBox.Show("特殊字符框中不能包含 大小写、数字、空格、汉字、全角 字符");
+                    bolInclNonOthers = true;
+                    break;
+                }
+            }
+            if (!bolInclNonOthers)
+            {
+                BtnCopy_10Prv.Enabled = true;
+                string strString = "", strPassword = "";
+                int n;
+                Random randUpper = new();
+                Random randLower = new();
+                Random randNum = new();
+                Random randOthers = new();
+                Random randString = new();
+                int nUpper, nLower, nNum, nOthers;//随机生成的整数
+                string subStrStringUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ", //大写
+                    subStrStringLower = "abcdefghijklmnopqrstuvwxyz", //小写
+                    subStrStringNum = "0123456789", //数字
+                    subStrStringOthers = TxtOthers_10Prv.Text;//其他字符
+                string subCharUpper, subCharLower, subCharNum, subCharOthers;
+
+                nUpper = randUpper.Next(subStrStringUpper.Length);
+                subCharUpper = subStrStringUpper.Substring(nUpper, 1);
+                nLower = randLower.Next(subStrStringLower.Length);
+                subCharLower = subStrStringLower.Substring(nLower, 1);
+                nNum = randNum.Next(subStrStringNum.Length);
+                subCharNum = subStrStringNum.Substring(nNum, 1);
+                nOthers = randOthers.Next(subStrStringOthers.Length);
+                subCharOthers = subStrStringOthers.Substring(nOthers, 1);//分别从大写、小写、数字、其他字符中随机取出的一个字符
+
+                nUpper = randUpper.Next(Decimal.ToInt32(NumLen_10Prv.Value));
+                nLower = randLower.Next(Decimal.ToInt32(NumLen_10Prv.Value));
+                nNum = randNum.Next(Decimal.ToInt32(NumLen_10Prv.Value));
+                nOthers = randOthers.Next(Decimal.ToInt32(NumLen_10Prv.Value));//随机生成的不大于密码长度整数
+
+                if (ChkUpper_10Prv.Checked)
+                {
+                    strString += subStrStringUpper;
+                    while ((nUpper == nLower) | (nUpper == nNum) | (nUpper == nOthers))
+                        nUpper = randUpper.Next(1, Decimal.ToInt32(NumLen_10Prv.Value));
+                }
+                else
+                    nUpper = 0;
+                if (ChkLower_10Prv.Checked)
+                {
+                    strString += subStrStringLower;
+                    while ((nLower == nUpper) | (nLower == nNum) | (nLower == nOthers))
+                        nLower = randLower.Next(1, Decimal.ToInt32(NumLen_10Prv.Value));
+                }
+                else
+                    nLower = 0;
+                if (ChkNum_10Prv.Checked)
+                {
+                    strString += subStrStringNum;
+                    while ((nNum == nUpper) | (nNum == nLower) | (nNum == nOthers))
+                        nNum = randNum.Next(1, Decimal.ToInt32(NumLen_10Prv.Value));
+                }
+                else
+                    nNum = 0;
+                if (ChkOthers_10Prv.Checked)
+                {
+                    strString += subStrStringOthers;
+                    while ((nOthers == nUpper) | (nOthers == nLower) | (nOthers == nNum))
+                        nOthers = randOthers.Next(1, Decimal.ToInt32(NumLen_10Prv.Value));
+                }
+                else
+                    nOthers = 0;
+                //生成包括随机密码可用的所有字符串strString，并确保随机生成的整数不相等
+
+                for (int i = 1; i <= Decimal.ToInt32(NumLen_10Prv.Value); i++)//按照用户选择的密码长度生成随机密码循环
+                {
+                    n = randString.Next(1, strString.Length);//随机生成不大于strString长度的一个整数
+                    if (i == nUpper)
+                        strPassword += subCharUpper;
+                    else if (i == nLower)
+                        strPassword += subCharLower;
+                    else if (i == nNum)
+                        strPassword += subCharNum;
+                    else if (i == nOthers)
+                        strPassword += subCharOthers;
+                    else
+                        strPassword += strString.Substring(n, 1);//从strString中随机取出一个字符
+                }
+
+                //     Random rand2 = new();
+                /*  if (ChkUpper_10Prv.Checked)
+                  {
+                      subStrString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                      StrString += subStrString;
+                      n = rand.Next(1, subStrString.Length);
+                      //               subStrString = StrString.Substring(n2, 1);
+                      strPassword = genPass(NumLen_10Prv.Value, n, StrString, subStrString);
+                  }
+                  if (ChkLower_10Prv.Checked)
+                  {
+                      subStrString = "abcdefghijklmnopqrstuvwxyz";
+                      StrString += subStrString;
+                      n2 = rand.Next(1, subStrString.Length);
+                      subStrString = StrString.Substring(n2, 1);
+                  }
+                  if (ChkNum_10Prv.Checked)
+                  {
+                      subStrString = "0123456789";
+                      StrString += subStrString;
+                      n2 = rand.Next(1, subStrString.Length);
+                      subStrString = StrString.Substring(n2, 1);
+                  }
+                  if (ChkOthers_10Prv.Checked)
+                  {
+                      StrString += TxtOthers_10Prv.Text;
+                      n2 = rand.Next(1, StrString.Length);
+                      subStrString = StrString.Substring(n2, 1);
+                  }
+                */
+                TxtPassword_10Prv.Text = strPassword;
+                BtnGen_10Prv.Text = "重新生成随机密码(&G)";
+            }
+        }
+
+        private void DisableChkBox(bool bolUpper, bool bolLower, bool bolNum, bool bolOth)
+        {
+            if (!bolOth & !bolLower & !bolNum)
+                ChkUpper_10Prv.Enabled = false;
+            if (!bolUpper & !bolOth & !bolNum)
+                ChkLower_10Prv.Enabled = false;
+            if (!bolUpper & !bolLower & !bolOth)
+                ChkNum_10Prv.Enabled = false;
+            if (!bolUpper & !bolLower & !bolNum)
+                ChkOthers_10Prv.Enabled = false;
+        }
+        private void EnableChkBox()
+        {
+            ChkUpper_10Prv.Enabled = true;
+            ChkLower_10Prv.Enabled = true;
+            ChkNum_10Prv.Enabled = true;
+            ChkOthers_10Prv.Enabled = true;
+        }
+
+        private void TxtOthers_10Prv_TextChanged(object sender, EventArgs e)
+        {
+            BtnResetOth_10Prv.Enabled = true;
+        }
+
+        private void BtnResetOth_10Prv_Click(object sender, EventArgs e)
+        {
+            TxtOthers_10Prv.Text = "~!@#$%^&*+-/.,\\{}[]();:";
+            BtnResetOth_10Prv.Enabled = false;
+        }
+
+        /*
+        /// <summary>
+        /// 生成随机密码函数
+        /// </summary>
+        /// <param name="value">密码长度，由NumericUpDown控件被用户选择</param>
+        /// <param name="n2">随机生成不大于subStrString长度的一个整数</param>
+        /// <param name="StrString">密码可用的所有字符</param>
+        /// <param name="subStrString">密码必用的所有字符</param>
+        /// <returns></returns>
+        private string genPass(decimal value, int n2, string StrString, string subStrString)
+        {
+            string strPassword = "";
+            int n1 = 0;
+            Random rand = new();
+            for (int i = 1; i <= Decimal.ToInt32(value); i++)
+            {
+                if (i != n2)
+                {
+                    n1 = rand.Next(1, StrString.Length);
+                    strPassword = StrString.Substring(n1, 1);
+                }
+                else
+                    strPassword = subStrString.Substring(n2, 1);
+            }
+            return strPassword;
+        }
+        */
+    }
+}
